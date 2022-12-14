3a32fd69e90d154b52733c569fbe69868c6a995f
Alaojo <halao.joshua@gmail.com>
2022-12-14 12:31:19 -0800

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,17 +1,14 @@
 #include "main.h"
 
 /**
- * main - print single digits in base 10
- * Return: Always 0
+ * _abs - computes the absolute value of an integer
+ * @t: The integer to be computed
+ * Return: The absolute value
  */
-int main(void)
+int _abs(int t)
 {
-	int num;
-
-	for (num = 0; num < 10; num++)
-		putchar((num % 10) + '0');
-
-	putchar('\n');
-
-	return (0);
+	if (t >= 0)
+		return (t);
+	else
+		return (-t);
 }
