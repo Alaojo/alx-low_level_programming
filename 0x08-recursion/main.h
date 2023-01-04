f1f705bf1ffc1831d0accafec57f06cc5996715a
Alaojo <halao.joshua@gmail.com>
2023-01-04 13:05:40 -0800

    0-puts_recursion.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -5,7 +5,7 @@
  * File: main.h
  * Auth: me
  * Desc: Header file containing prototypes for all functions
- * used in the 0x07-recursion directory
+ *	 used in the 0x07-recursion directory.
  */
 
 int _putchar(char c);
